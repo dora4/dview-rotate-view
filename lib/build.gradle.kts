664236d10d489f0d73a668ea20e9d4b1fd97db19
跟着哆啦学安卓 <71242257+dora4@users.noreply.github.com>
2026-05-08 11:23:12 +0700

    Update build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -34,7 +34,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = rootProject.project.name
-                version = "1.9"
+                version = "1.10"
             }
         }
     }
